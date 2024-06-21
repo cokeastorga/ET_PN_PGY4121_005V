e5f1ce57c65d1c318473ba0f2ab6bf01f443fdc2
cokeastorgac <jor.astorga@duocuc.cl>
2024-06-20 20:53:25 -0400

    AppWeb

---
+++ ObjetosPerdidos/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.constanza.objetosperdidos',
+  appName: 'ObjetosPerdidos',
+  webDir: 'www'
+};
+
+export default config;
---
+++ ObjetosPerdidos/src/app/models/objeto.ts
@@ -0,0 +1,7 @@
+export interface Objeto {
+    titulo:string;
+    descripcion:string;
+    ubicacion:string;
+    fecha_encontrado:string;
+    imagen:string;
+}
