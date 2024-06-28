0765c1069bcf0deecc3dad249cc4288b1523a57b
cokeastorgac <jor.astorga@duocuc.cl>
2024-06-28 16:58:27 -0400

    firebaseWeb

--- Code/src/environments/environment.ts
+++ Code/src/environments/environment.ts
@@ -13,6 +13,14 @@ export const environment = {
     appId: "1:926605195957:web:ff806d41981266bfc49cfc",
     measurementId: "G-KHJ2NBKYKJ"
   },
+  firebaseConfigOtraBD : {
+    apiKey: "AIzaSyBlyNNeP35CwnX6aVOPkXvKZgfplABFEAQ",
+    authDomain: "objetosperdidos-54c6f.firebaseapp.com",
+    projectId: "objetosperdidos-54c6f",
+    storageBucket: "objetosperdidos-54c6f.appspot.com",
+    messagingSenderId: "217039724142",
+    appId: "1:217039724142:web:07a5179c5025eb89d0107f"
+  },
   production: false,
 };
 
